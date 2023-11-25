91ff6b6b44e5b9759f2817716c51af1e626ff544
larry659 <larryfunmbi26@gmail.com>
2023-11-25 20:17:15 +0100

    make this a git repository

---
+++ src/main/java/com/example/luxecrib/LuxeCribApplication.java
@@ -0,0 +1,13 @@
+package com.example.luxecrib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxeCribApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxeCribApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luxecrib/LuxeCribApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luxecrib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxeCribApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

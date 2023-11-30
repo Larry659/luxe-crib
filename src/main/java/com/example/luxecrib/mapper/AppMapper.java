ab848bcf26c701c8d5e0b7f7e8a8576007ec7387
larry659 <larryfunmbi26@gmail.com>
2023-11-30 11:23:20 +0100

    mapStruct dependency and interface creation

---
+++ src/main/java/com/example/luxecrib/mapper/AppMapper.java
@@ -0,0 +1,9 @@
+package com.example.luxecrib.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface AppMapper {
+   // SimpleDestination sourceToDestination(SimpleSource source);
+    //SimpleSource destinationToSource(SimpleDestination destination);
+}
